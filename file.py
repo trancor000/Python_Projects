58a0015452bb218d0e6baec6a4ec776ce5b980b1
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-17 16:48:29 -0700

    file.py

---
+++ file.py
@@ -0,0 +1,64 @@
+
+import sqlite3
+
+conn = sqlite3.connect('files.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_documents( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_docs TEXT, \
+        col_texts TEXT, \
+        col_pdfs TEXT \
+        )")
+    conn.commit()
+conn.close()
+
+
+conn = sqlite3.connect('files.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_documents(col_docs, col_texts, col_pdfs) VALUES (?, ?, ?)",
+                ('information.docx', ('Hello.txt', 'World.txt'), 'data.pdf'))
+    conn.commit()
+conn.close()
+
+
+conn=sqlite3.connect('files.db')
+
+with conn:
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_pictures( \
+        ID STRING, \
+        col_pics PNG, \
+        col_sound MPG \
+        col_jpg JPG \
+        )")
+    conn.commit()
+conn.close()
+
+
+conn=sqlite3.connect('files.db')
+
+with conn:
+    cur=conn.cursor()
+    cur.execute("INSERT INTO tbl_documents(col_docs) VALUES (?)", \
+                ('information.docx'))
+    cur.execute("INSERT INTO tbl_documents(col_texts) VALUES (?, ?)", \
+                ('Hello.txt', 'World.txt'))
+    cur.execute("INSERT INTO tbl_documents(col_pdfs) VALUES (?)", \
+                ('data.pdf'))
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('files.db')
+
+
+with conn:
+    cur=conn.cursor()
+    cur.execute("SELECT col_texts FROM tbl_documents")
+    varDocs=cur.fetchall()
+    for item in varDocs:
+        msg=r.varDocs
+    print(msg)
