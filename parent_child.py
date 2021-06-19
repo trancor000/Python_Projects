122674abc90e229ff7a5b035afe4f039b6e6a10c
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-18 19:07:07 -0700

    Update

--- parent_child.py
+++ parent_child.py
@@ -1,14 +1,14 @@
-def parent():
-    class Employee:
-        name = 'No Name Provided'
+def parent():  # define function
+    class Employee:  # instantiate parent class
+        name = 'No Name Provided'  # list attributes
         position = ' '
 
-    class Manager(Employee):
-        department = 'Kitchen'
+    class Manager(Employee):  # instantiate child class
+        department = 'Kitchen'  # list attributes
         directs = 6
 
-    class Leader(Employee):
-        years = 4
+    class Leader(Employee):  # instantiate second child class
+        years = 4  # list attributes
         pay = 18.00
 
 
