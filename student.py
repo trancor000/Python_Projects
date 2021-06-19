027eb4a419fcc7ee058f609117ec5df46e67c668
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-18 21:13:14 -0700

    student.py

---
+++ student.py
@@ -0,0 +1,34 @@
+class Person:
+    name = "Corey Tran"
+    user = "Ctran.prosper@gmail.com"
+    password = "8675309"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_user = input("Enter your user: ")
+        entry_password = input("Enter your password: ")
+        if (entry_user == self.user and entry_password == self.password):
+            print("Login complete, {}".format(entry_name))
+        else:
+            print("The user or password is incorrect.")
+
+
+class Student(Person):
+    course_name = "Python"
+    enrollment_weeks = "15 weeks"
+    program_code = "1234"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_user = input("Enter your user: ")
+        entry_code = input("Enter your code: ")
+        if (entry_user == self.user and entry_code == self.program_code):
+            print("Login complete, {}".format(entry_name))
+        else:
+            print("User or code is incorrect")
+
+    instructor = Person()
+    instructor.getLoginInfo()
+
+    pupil = Student()
+    pupil.getLoginInfo()
