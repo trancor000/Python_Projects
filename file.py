19db2a47a338fb89c80a0faa562745bab0e3121b
trancor000 <83535306+trancor000@users.noreply.github.com>
2021-06-20 16:57:25 -0700

    Update

--- file.py
+++ file.py
@@ -2,14 +2,17 @@
 import sqlite3
 
 conn = sqlite3.connect('files.db')
-
-mycursor.execute("CREATE TABLE documents (txts VARCHAR(255))")
+with conn:
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS documents (txts VARCHAR(255))")
+    conn.close()
 
 with conn:
     fileList = ('information.docx', 'Hello.txt', 'myImage.png',
                 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
     txtList = ''
-    for item in file_List:
+    for item in fileList:
         if item.endswith(".txt"):
             cur = conn.cursor()
             cur.execute("INSERT INTO documents(col_txts) VALUES (?)",
